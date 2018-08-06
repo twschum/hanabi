2d3519db14748c31b3cf31556fbf5602ce4a52ca
Tim <tim.schumacher@nokia.com>
2018-08-05 21:55:46 -0400

    Factored out Board module

---
+++ pkg/board/board.go
@@ -0,0 +1,46 @@
+/* board
+
+Board
+
+*/
+package board
+
+import (
+	"github.com/twschum/hanabi/pkg/card"
+	"fmt"
+)
+
+type Board struct {
+	Strikes, Information int
+	//draw // one array for deck, everything else is a slice or pointer
+	//discard
+	Fireworks [card.SuitCount]int // starts at 0 for each suit
+}
+
+func (board *Board) PlayCard(c *card.Card) bool {
+	// use closure to reprint board??
+	// returns success or not
+	if c.Number == board.Fireworks[c.Color] + 1 {
+		board.Fireworks[c.Color] = c.Number
+	} else {
+		// card to discards
+		board.Strikes++
+		fmt.Println("Failed to play %v", c)
+		return false
+	}
+	fmt.Println("Successfully played %v", c)
+	if c.Number == 3 || c.Number == 5 {
+		board.Information++
+	}
+	return true
+}
+
+func (board *Board) Print() {
+	// Prints out the game information
+	fmt.Printf("R B G Y W\n")
+	for i := range board.Fireworks {
+		fmt.Printf("%d ", board.Fireworks[i])
+	}
+	fmt.Println("")
+}
+
